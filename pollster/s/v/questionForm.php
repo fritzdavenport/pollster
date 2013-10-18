9e0a5f40e21fc433ac522c3fb01baa6760f7429d
Fritz Davenport <cdavenp1@uvm.edu>
2013-10-17 22:07:42 -0400

    syntax

--- pollster/s/v/questionForm.php
+++ pollster/s/v/questionForm.php
@@ -3,8 +3,8 @@
 		<input type=hidden name="fldQuestionNumber" value=<? echo '"'.$qn.'"'; ?> >
 		<INPUT TYPE="text" name="fldQuestionText" placeholder="Please input question text">
 		<SELECT name="numAnswers">
-		<? 	for ($i=2; $i < 10; $i++) { ?> 
-			<OPTION <? if ($i == 4){ echo "selected";?> > <? echo $i ?> </OPTION>
+		<? 	for ($i=2; $i < 10; $i++) { 3?> 
+			<OPTION <? if ($i == 4){ echo "selected"; }?> > <? echo $i ?> </OPTION>
 		<? } ?>
 		</SELECT> How many answers will it have?
 	</p>
